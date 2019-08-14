2e0c5df530c2951627436ea7dff614e0b10c0378
Dirk Lellinger <dlellinger@users.sourceforge.net>
2019-08-14 21:55:34 +0200

    Change namespaces and project names to HtmlToFlowDocument

--- HtmlToFlowDocument.Wpf/Rendering/WpfRenderer.cs
+++ HtmlToFlowDocument.Wpf/Rendering/WpfRenderer.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HtmlToXamlConverter.Dom;
+using HtmlToFlowDocument.Dom;
 using swd = System.Windows.Documents;
 
-namespace HtmlToXamlConverter.Rendering
+namespace HtmlToFlowDocument.Rendering
 {
   public class WpfRenderer
   {
